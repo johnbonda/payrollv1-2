531d2e8f0e2ccfa664c414d4481c5cf60aa0cea0
johnbonda <john@belfricsbt.com>
2019-01-01 16:59:55 +0530

    Changed instance

--- utils/constants.js
+++ utils/constants.js
@@ -5,8 +5,8 @@ module.exports = {
   URL: "http://localhost:9305",
   URI: "http://54.254.174.74:8080",
   URX: "http://54.254.174.74:8080",
-  CRX: "http://18.188.23.5:9305/api/dapps/2197e1a71e1105989b207d81d67f6a882dc14d02dc8ea4cd4ac060a7b2f0ecb9",
-  LSR: "http://18.188.23.5:9305/api/dapps/",
+  CRX: "http://54.157.252.226:9305/api/dapps/048c1869547f60db8a6cd93b06a786abe5f50c4567198f4df0376462f9a20c5e",
+  LSR: "http://54.157.252.226:9305/api/dapps/",
   MRI: "http://52.201.227.220:8080/sendMail/",
   fees: {
     send: 0.001,
