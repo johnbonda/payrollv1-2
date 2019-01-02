1a08f89ff2ed6c77d81a611d32711a51bda29333
johnbonda <john@belfricsbt.com>
2019-01-02 16:42:17 +0530

    fixed bug

--- utils/logger.js
+++ utils/logger.js
@@ -1,6 +1,6 @@
 var log4js = require( "log4js" );
 log4js.configure({
-  appenders: { dappLogs: { type: 'file', filename: './logs/PayrollDapp/dappLogs.log' } },
+  appenders: { dappLogs: { type: 'dateFile', filename: './logs/PayrollDapp/dappLogs.log', daysToKeep: 1 } },
   categories: { default: { appenders: ['dappLogs'], level: 'info' } }
 });
 module.exports = log4js.getLogger( "dappLogs" );
