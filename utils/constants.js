0f2e69b5813c6b56a85f1acf503258c34546479b
johnbonda <john.bonda7@gmail.com>
2019-05-29 14:53:24 +0530

    Changed the configuration files at one place

--- utils/constants.js
+++ utils/constants.js
@@ -1,13 +1,15 @@
+var config = require("../config.json");
+
 module.exports = {
   fixedPoint : Math.pow(10, 10),
   defaultCurrency: 'BEL', // default currency symbole for Belrium
   totalSupply: 2100000000000000000,
   URL: "http://localhost:9305",
-  URI: "http://54.254.174.74:8080",
-  URX: "http://54.254.174.74:8080",
-  CRX: "http://localhost:9305/api/dapps/048c1869547f60db8a6cd93b06a786abe5f50c4567198f4df0376462f9a20c5e",
+  URI: config.bkvs,
+  URX: config.bkvs,
+  CRX: "http://localhost:9305/api/dapps/" + config.superdapp ,
   LSR: "http://localhost:9305/api/dapps/",
-  MRI: "http://52.201.227.220:8080/sendMail/",
+  MRI: config.centralServer + "/sendMail/",
   fees: {
     send: 0.001,
     inTransfer: 0.001,
